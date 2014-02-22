8279f9ee81b6af99a35d14fe6c5efee0d2992e82
Rocky Camacho <rockycamacho@Rockys-MacBook-Pro.local>
2014-02-22 17:21:31 +0800

    Initial Commit

---
+++ NPlayerReactor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NPlayerReactor
+//
+//  Created by Rocky Camacho on 2/22/14.
+//  Copyright (c) 2014 Rocky Camacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NPlayerReactor/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NPlayerReactor
+//
+//  Created by Rocky Camacho on 2/22/14.
+//  Copyright (c) 2014 Rocky Camacho. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
