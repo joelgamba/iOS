4599672e43c73b5b261cf543620d8aa1d3f58ce6
lamito <alagadnibabidy@gmail.com>
2014-02-24 16:01:18 +0800

    Reactor Color
    Implementation of Reactor Color
    - lacks winner and timer

--- NPlayerReactor/ViewController.m
+++ NPlayerReactor/ViewController.m
@@ -17,11 +17,14 @@
 @property (weak, nonatomic) IBOutlet UILabel *player2Score;
 @property (weak, nonatomic) IBOutlet UILabel *player3Score;
 @property (weak, nonatomic) IBOutlet UILabel *player4Score;
+@property (weak, nonatomic) IBOutlet UILabel *colorText;
 
 @property int score1;
 @property int score2;
 @property int score3;
 @property int score4;
+@property int indexColor;
+@property int indexText;
 
 @end
 
@@ -31,7 +34,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
+    [self changeLabel];
 }
 
 - (void)didReceiveMemoryWarning
@@ -47,29 +50,73 @@
 
 - (IBAction)onPlayerOneClick:(id)sender {
     NSLog(@"Player 1 click");
-    self.score1++;
+    if([self isCorrect]){
+        self.score1++;
+    }else{
+        self.score1--;
+    }
     [self.player1Score setText:[NSString stringWithFormat: @"%d", self.score1]];
 }
 
 - (IBAction)onPlayerTwoClick:(id)sender {
     NSLog(@"Player 2 click");
-    self.score2++;
+    if([self isCorrect]){
+        self.score2++;
+    }else{
+        self.score2--;
+    }
     [self.player2Score setText:[NSString stringWithFormat: @"%d", self.score2]];
 
 }
 
 - (IBAction)onPlayerThreeClick:(id)sender {
     NSLog(@"Player 3 click");
-    self.score3++;
+    if([self isCorrect]){
+        self.score3++;
+    }else{
+        self.score3--;
+    }
     [self.player3Score setText:[NSString stringWithFormat: @"%d", self.score3]];
 
 }
 
 - (IBAction)onPlayerFourClick:(id)sender {
     NSLog(@"Player 4 click");
-    self.score4++;
+    [self changeLabel];
+    if([self isCorrect]){
+        self.score4++;
+    }else{
+        self.score4--;
+    }
+    
+
     [self.player4Score setText:[NSString stringWithFormat: @"%d", self.score4]];
 
+}
+
+- (void)changeLabel{
+    self.indexColor = arc4random()%3;
+    self.indexText = arc4random() %3;
+    
+    NSArray *colorArray = [[NSArray alloc] initWithObjects:[UIColor redColor],[UIColor blueColor],[UIColor greenColor], nil ];
+    NSArray *textArray = [[NSArray alloc] initWithObjects:@"Red",@"Blue",@"Green", nil];
+    NSLog([NSString stringWithFormat:@"%d %d %@", self.indexColor, self.indexText, textArray[self.indexText]]);
+    
+    [self.colorText setText:textArray[self.indexText]];
+    [self.colorText setTextColor:colorArray[self.indexColor]];
+}
+
+-(BOOL)isCorrect{
+    BOOL toReturn;
+    
+    
+    if (self.indexColor == self.indexText) {
+        toReturn=YES;
+    }else{
+        toReturn=NO;
+    }
+    [self changeLabel];
+    return toReturn;
 }
 
 
